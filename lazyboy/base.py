a78d6360fb83f720307efd47c0ef520f74603f0b
Ian Eure <ian.eure@gmail.com>
2009-10-15 16:45:54 -0700

    Add connetions arg.

--- lazyboy/base.py
+++ lazyboy/base.py
@@ -12,17 +12,12 @@ import lazyboy.connection as connection
 class CassandraBase(object):
     """The base class for all Cassandra-accessing objects."""
 
-    def __init__(self):
-        self._clients = {}
+    def __init__(self, connections):
+        self._keyspace = connection.init(connections)
 
     def _get_cas(self, keyspace=None):
         """Return the cassandra client."""
         if not keyspace and (not hasattr(self, 'key') or not self.key):
             raise ErrorIncompleteKey("Instance has no key.")
 
-        keyspace = keyspace or self.key.keyspace
-        if keyspace not in self._clients:
-            self._clients[keyspace] = connection.get_pool(keyspace)
-
-        return self._clients[keyspace]
-
+        return self._keyspace(keyspace or self.key.keyspace)
